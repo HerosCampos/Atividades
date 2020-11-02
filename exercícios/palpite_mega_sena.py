db29179a0a414cdf9fb55341fe697989f60567e4
HerosCampos <herosrac@gmail.com>
2020-11-02 20:18:12 -0300

    Palpites da Mega Sena

--- exercícios/palpite_mega_sena.py
+++ exercícios/palpite_mega_sena.py
@@ -1,4 +1,23 @@
 """
-Exercício 88 do canal Curso em Vídeo do professor Gustavo Guanabara
-
+Exercício 88 do curso de Python do canal Curso em Vídeo do professor Gustavo Guanabara
+Faça um programa que ajude um jogador da MEGASENA a criar palpites. O promgrama vai perguntar
+quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando 
+tudo em uma lista composta.
 """
+from random import randrange
+
+jogo = []
+jogos = []
+cont = 0
+
+opc = int(input('Quantos jogos você quer sortear? '))
+
+while cont < opc:
+    while len(jogo) < 6:
+        rand = randrange(61)
+        if rand not in jogo:
+            jogo.append(rand)
+    print(jogo)
+    jogos.append(jogo[:])
+    jogo.clear()
+    cont += 1
