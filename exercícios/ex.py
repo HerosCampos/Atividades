ab738634870f7ea05f51df62f9bff681aae5dd0a
HerosCampos <herosrac@gmail.com>
2020-11-06 16:31:24 -0300

    Update ex.py

--- exercícios/ex.py
+++ exercícios/ex.py
@@ -1,3 +1,24 @@
+from random import randrange
+from time import sleep
+
+def sorteia(lista):
+    print('Os números sorteados foram: ', end='')
+    for cont in range(0, 5):
+        n = randrange(10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.5)
+    print()
+    
+
+def somaPar(lista):
+    soma = 0
+    for v in lista:
+        if v % 2 == 0:
+            soma += v
+    print(f'Somando os valores pares de {lista}: temos {soma}')
 
 
-
+nums = list()
+sorteia(nums)
+somaPar(nums)
