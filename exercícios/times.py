01257edeecc2efc9071de55d0f1e459fbf270d35
Heros <herosrac@gmail.com>
2021-11-19 00:15:02 -0300

    Tabela de times do brasileirão série b 2021

---
+++ exercícios/times.py
@@ -0,0 +1,6 @@
+tabela = ('Botafogo', 'Coritiba', 'Goias', 'Guarani', 'Avai', 'CRB', 'Nautico', 'Vasco da Gama', 'Sampaio Correa', 'Cruzeiro', 'Operario', 'Vila Nova', 'Ponte Preta', 'Brusque', 'Remo', 'Londrina', 'Vitoria', 'Confianca', 'Brasil de pelotas')
+
+print(f'Times do Brasileirão Série B: {tabela}')
+print(f'Os 5 primeiros times são: {tabela[0:5]}')
+print(f'Os 4 últimos são: {tabela[16:]}')
+print(f'Os times em ordem alfabética: {sorted(tabela)}')
