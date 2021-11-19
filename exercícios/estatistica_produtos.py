3d36d95d30c51d8b252acbe777c315b74bc5fad0
Heros <herosrac@gmail.com>
2021-11-19 00:07:06 -0300

    Estatistica de produtos com python

--- exercícios/estatistica_produtos.py
+++ exercícios/estatistica_produtos.py
@@ -1,4 +1,35 @@
 
 print('-' * 30)
-print('LOJA SUPER BARATÃO')
+print(f'{"LOJA SUPER BARATÃO":^30}')
 print('-' * 30)
+soma = mais_de_1000 = barato = cont = 0
+barato_nome = ""
+
+while True:
+    nome = input('Nome do produto: ')
+    preco = float(input('Preço: '))
+
+    soma += preco
+
+    if preco > 1000:
+        mais_de_1000 += 1
+    
+    if cont == 0:
+        barato = preco
+        barato_nome = nome
+    else:
+        if preco < barato:
+            barato = preco
+            barato_nome = nome
+
+
+    cont = " "
+    while cont not in "SsNn":
+        cont = input('Quer continuar? [S/N] ')
+    if cont in "Nn":
+        break
+
+
+print(f'O total da compra foi R${soma:.2f}')
+print(f'Temos {mais_de_1000} produtos custando mais de R$ 1000.00')
+print(f'O produto mais barato foi {barato_nome} que custa R$ {barato:.2f}')
