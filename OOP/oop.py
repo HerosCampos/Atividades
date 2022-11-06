bea19001ec199b01dd28886dc016aa71d988c24a
Heros Campos <herosrac@gmail.com>
2022-11-06 17:10:02 -0300

    Practicing OOP with python

---
+++ OOP/oop.py
@@ -0,0 +1,48 @@
+class Student:
+    def __init__(self, new_name, new_grades):
+        # Properties
+        self.name = new_name
+        self.grades = new_grades
+
+    # Method
+    def average(self):
+        return sum(self.grades) / len(self.grades) 
+
+
+student_one = Student("Rolf Smith", [70, 88, 90, 99])
+print(student_one.name)
+print(student_one.average())
+
+
+################################################################################
+
+
+class Garage:
+    def __init__(self):
+        self.cars = []
+
+    # We que do for loops once we have __len__ and __getitem__ methods implemented
+    def __len__(self):
+        return len(self.cars)
+
+    def __getitem__(self, i):
+        return self.cars[i]
+
+    def __repr__(self):
+        return f"<Garage {self.cars}>"    
+
+#    def __str__(self):
+#        return f"Garage with {len(self)} cars."
+
+
+ford = Garage()
+ford.cars.append("Ford")
+ford.cars.append("Fusion")
+print(ford)
+#print(len(ford))
+#for car in ford:
+#    print(car)
+
+
+
+
